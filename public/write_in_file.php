9cbf7157a00994a904f1aa791e946bf2ee506749
tchikl_h <herve.tchikladze@epitech.eu>
2018-05-01 00:17:54 +0200

    fix external css file mailchimp

--- public/write_in_file.php
+++
@@ -1,9 +0,0 @@
-<?php
-header('Location: http://localhost/landingPage.html#contact');
-$file =$_GET['Email'].'.txt';
-// Ajoute une personne
-$current = "From : ".$_GET['Name']."\nEmail : ".$_GET['Email']."\nMessage : ".$_GET['Message'];
-// Écrit le résultat dans le fichier
-file_put_contents($file, $current);
-echo "Merci de message"
-?>
